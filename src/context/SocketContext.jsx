1e36b857f84360a9b20e9a20a34fb8ecbfbe1631
Arya262 <Aryarahul262@gmail.com>
2025-07-07 10:55:44 +0530

    07-07-25  commit

--- src/context/SocketContext.jsx
+++ src/context/SocketContext.jsx
@@ -1,22 +1,15 @@
 import React, { createContext, useContext, useEffect, useState } from "react";
 import { io } from "socket.io-client";
-import { useAuth } from "./AuthContext";
 
 const SocketContext = createContext(null);
 
 export const SocketProvider = ({ children }) => {
-  const { user } = useAuth();
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    if (!user?.token) return;
-    const API_BASE = import.meta.env.VITE_API_BASE;
-    const newSocket = io(API_BASE, {
+    const newSocket = io("https://marketing-uoxu.onrender.com", {
       transports: ["websocket"],
       withCredentials: true, // ✅ Send cookies (like auth_token)
-      auth: {
-        token: user.token, // ✅ Send the auth token
-      },
     });
 
     newSocket.on("connect", () => {
@@ -30,7 +23,7 @@ export const SocketProvider = ({ children }) => {
     setSocket(newSocket);
 
     return () => newSocket.disconnect();
-  }, [user?.token]);
+  }, []);
 
   return (
     <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
