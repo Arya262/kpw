83856b0cfccb0a4fd0b242a7e2b17f1e28b53644
Kpsingh0-7 <142295909+Kpsingh0-7@users.noreply.github.com>
2025-07-10 11:31:06 +0530

    Added new files

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-
+import path from 'path';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      'jspdf': path.resolve(__dirname, 'node_modules/jspdf/dist/jspdf.umd.js'),
+    },
+  },
   server: {
     host: '0.0.0.0',
-    
   },
 });
