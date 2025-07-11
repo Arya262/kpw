2fd630de4356b4a9a6b7722fd9e368ef962df324
Kpsingh0-7 <142295909+Kpsingh0-7@users.noreply.github.com>
2025-07-11 10:22:48 +0530

    11-07-25

--- src/config/api.jsx
+++ src/config/api.jsx
@@ -1,5 +1,5 @@
 export const API_BASE =
-  import.meta.env.VITE_API_BASEz || "https://www.foodchow.app";
+  import.meta.env.VITE_API_BASE || "https://www.foodchow.app";
 
 export const API_ENDPOINTS = {
   BROADCASTS: {
